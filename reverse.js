c3a3008da9fda0eb26e0e9942e56cf5c6329f323
Prafullata Sonawane <prafullatass@gmail.com>
2019-07-09 14:22:35 -0500

    Added integer reverse function

--- reverse.js
+++ reverse.js
@@ -9,3 +9,12 @@ var str = "this is a test program";
 
 str = str.split(' ').reverse().join(" ");
 console.log(str);
+
+
+//reverse numbers by keeping sign same
+
+var num = -12332;
+var reverseNum =
+(Math.abs(num).toString().split('').reverse().join('')) * Math.sign(num);
+
+console.log(reverseNum);
