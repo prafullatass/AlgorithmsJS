67a6a459f194971b532da74c39b372c611b1ec42
Prafullata Sonawane <prafullatass@gmail.com>
2019-07-25 22:37:58 -0500

    new algorithm

---
+++ SumOfArray.js
@@ -0,0 +1,3 @@
+//You will be given an array of several arrays that each contain integers and your goal is to
+//write a function that will sum up all the numbers in all the arrays. For example,if the
+//input is [[3, 2], [1], [4, 12]] then your program should output 22 because 3 + 2 + 1 + 4 + 12 = 22.
