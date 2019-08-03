6e08c72f352f6416095ca6558ff93f546c6292ef
Prafullata Sonawane <prafullatass@gmail.com>
2019-08-02 23:16:22 -0500

    adding heap sort

---
+++ HeapSort.js
@@ -0,0 +1,7 @@
+/*
+* Build a max heap out of the array. A heap is a specialized tree like
+* data structure that satisfies the heap property. The heap property
+* for max heap is the following: "if P is a parent node of C, then the
+* key (the value) of node P is greater than the key of node C"
+* Source: https://en.wikipedia.org/wiki/Heap_(data_structure)
+*/
