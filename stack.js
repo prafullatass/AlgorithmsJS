90dbb20ec5885caa1eec38590ceb350b95711116
Prafullata Sonawane <prafullatass@gmail.com>
2019-08-02 23:43:19 -0500

    implemented push method in stack

--- stack.js
+++ stack.js
@@ -1,1 +1,23 @@
 // implement stack
+
+var Stack = (function () {
+
+    function Stack() {
+      //The top of the Stack
+      this.top = 0;
+      //The array representation of the stack
+      this.stack = new Array();
+    }
+
+    //Adds a value onto the end of the stack
+    Stack.prototype.push = function (value) {
+      this.stack[this.top] = value;
+      this.top++;
+    };
+    return Stack;
+}());
+
+var newStack = new Stack();
+
+newStack.push(9);
+console.log(newStack);
