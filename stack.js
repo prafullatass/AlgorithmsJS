bdd09b3e1f8e36919ce9f9ed92f1959fe9096b80
Prafullata Sonawane <prafullatass@gmail.com>
2019-08-02 23:57:31 -0500

    completed stack

--- stack.js
+++ stack.js
@@ -15,8 +15,17 @@ var Stack = (function () {
       this.top++;
     };
     Stack.prototype.pop = function() {
+        if(this.top === 0)
+            return("Stack is Empty!!");
         this.top--;
         return(this.stack[this.top]);
+    }
+    Stack.prototype.view = function () {
+        if(this.top === 0)
+            return("Stack is Empty!!");
+        console.log("Stack -----")
+        for(i=0; i<this.top; i++)
+            console.log(this.stack[i]);
     }
     return Stack;
 }());
@@ -25,6 +34,12 @@ var newStack = new Stack();
 
 newStack.push(9);
 newStack.push(5);
+newStack.view();
 console.log(newStack.pop());
 newStack.push(3);
-console.log(newStack);
+newStack.view();
+console.log(newStack.pop());
+console.log(newStack.pop());
+newStack.view();
+console.log(newStack.pop());
+
