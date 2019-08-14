e2a6545d770419a0e328ab2de1c8a7bc687d7b65
Prafullata Sonawane <prafullatass@gmail.com>
2019-08-13 19:50:22 -0500

    added algorithms to eliminate duplicate from an array

---
+++ Duplicate.js
@@ -0,0 +1,20 @@
+// eliminate duplicate no from array
+//always use hash table
+
+const duplicate = arr => {
+    var hashTable = {};
+    for (let i = 0; i < arr.length; i++) {
+        if(hashTable[arr[i]] === 1)
+        {
+            var temp = arr.splice(i,1);
+            --i;
+        }
+        else
+            hashTable[arr[i]] = 1;
+    }
+    return arr;
+}
+
+const array = [3,5,3,2,34,6,2,6,3,56,4,87];
+console.log(array);
+console.log(duplicate(array));
